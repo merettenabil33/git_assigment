7452620b2b2f5f9540894a8a6a2cfa908219ca53
merettenabil33 <58133539+merettenabil33@users.noreply.github.com>
2019-11-25 12:16:39 +0200

    Update TwoPowerN.java

--- git_assignment_initial-master/src/main/java/MathTopics/TwoPowerN.java
+++ git_assignment_initial-master/src/main/java/MathTopics/TwoPowerN.java
@@ -1,12 +1,12 @@
 package MathTopics;
-
+import git_assignment_initial.*;
 import git_assignment_initial.ITopic;
 
 //Please read CicleArea.java Code and implement as a clone of it but using your formula
 
 public class TwoPowerN implements ITopic {
 	public static long Get2PN(int n) {
-		return return (double)(2 * n);
+		 return (long)(2 * n);
 	}
 
 	@Override
